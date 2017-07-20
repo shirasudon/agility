1ca6ee85df1c69808e6851cef4c886093821c266
Hitoshi Otsuki <hitochan777@gmail.com>
2017-07-20 23:14:19 +0900

    Set sourceMap to true

--- fuse.js
+++ fuse.js
@@ -2,6 +2,7 @@ const fb = require("fuse-box");
 const fuseBox = new fb.FuseBox({
     homeDir: "src/client",
     output: "dist/$name.js",
+    sourceMaps: true
     plugins: [
         fb.BabelPlugin(),
     ],
