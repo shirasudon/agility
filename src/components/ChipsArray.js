798156cd1708d23b0059cd8fa5feb18a41e04bdf
Hitoshi Otsuki <hitochan777@gmail.com>
2017-08-10 07:08:21 +0900

    Implement friend filter and addition in create group modal

---
+++ src/components/ChipsArray.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import Chip from 'material-ui/Chip';
+
+const styleSheet = createStyleSheet(theme => ({
+    chip: {
+        margin: theme.spacing.unit / 2,
+    },
+    row: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+    },
+}));
+
+class ChipsArray extends Component {
+
+    styles = {
+        chip: {
+            margin: 4,
+        },
+        wrapper: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+    };
+
+    render() {
+        const {classes, chipData, handleRequestDelete} = this.props;
+
+        return (
+            <div className={classes.row}>
+                {chipData.map(data => {
+                    return (
+                        <Chip
+                            label={data.label}
+                            key={data.key}
+                            onRequestDelete={()=>{handleRequestDelete(data);}}
+                            className={classes.chip}
+                        />
+                    );
+                })}
+                </div>
+        );
+    }
+}
+
+ChipsArray.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styleSheet)(ChipsArray);
--- src/components/CreateGroupModal.js
+++ src/components/CreateGroupModal.js
@@ -1,29 +1,108 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+// import ChipInput from 'material-ui-chip-input';
 import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import Chip from 'material-ui/Chip';
+import TextField from 'material-ui/TextField';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+
+import ChipsArray from './ChipsArray';
 
 class CreateGroupModal extends Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            selectedUsers: [], 
+            searchText: ''
+        }
+
+        this.handleAddChip = this.handleAddChip.bind(this);
+        this.handleDeleteChip = this.handleDeleteChip.bind(this);
+        this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+    }
+
+    handleAddChip(username) {
+        const newSelectedUsers = this.state.selectedUsers.slice();
+        newSelectedUsers.push(username);
+        this.setState({selectedUsers: newSelectedUsers});
+    }
+
+    handleDeleteChip(data) {
+        const newSelectedUsers = this.state.selectedUsers.filter((u, index) => {
+            return u !== data.label;
+        });
+        this.setState({selectedUsers: newSelectedUsers});
+    }
+
+    handleSearchTextChange(event) {
+        this.setState({searchText: event.target.value});
     }
 
     render(){
-        const {showModal, closeModal} = this.props;
+        const {
+            showModal, 
+            closeModal, 
+            friends,
+        } = this.props;
+
+        const {selectedUsers, searchText} = this.state;
+
+        const matchedUsernames = friends.all.filter((username) => {
+            const friend = friends.byUsername[username];
+            const regex = new RegExp(searchText);
+            return regex.test(friend.username) && !selectedUsers.includes(username);
+        }); 
+
+        const chipData = selectedUsers.map((username, index) => {
+            return {
+                label: username,
+                key: index,
+            }
+        });
+
+        const matchedUserList = matchedUsernames.map((username, index) => {
+            return (
+                <ListItem button key={index}>
+                    <ListItemText
+                        primary={username}
+                        onClick={()=>{this.handleAddChip(username);}} 
+                    />
+                </ListItem>
+            );
+        });
+
         return (
             <Dialog open={showModal} onRequestClose={closeModal}>
                 <DialogTitle>グループ作成</DialogTitle>
                 <div>
                     <h4>友達を選んでください！</h4>
+                        <ChipsArray chipData={chipData} handleRequestDelete={this.handleDeleteChip}/>
+                        <TextField
+                            id="search-friend"
+                            value={searchText}
+                            InputProps={{ placeholder: 'Search Your friends!' }}
+                            onChange={this.handleSearchTextChange}
+                            fullWidth
+                            margin="normal"
+                       />
+
+                       {matchedUserList.length > 0 ? (
+                            <List>
+                                {matchedUserList}
+                            </List>
+                       ):
+                            <span>No matched User</span>
+                       }
                 </div>
             </Dialog>
         );
     }
 }
 
-const mapStateToProps = () => ({
-
+const mapStateToProps = ({friends}) => ({
+    friends: friends
 });
 
 export default connect(mapStateToProps)(CreateGroupModal);
