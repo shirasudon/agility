9219d6f6cd78b65109ecf7037922a13d8e445daa
Otsuki Hitoshi <hitochan777@gmail.com>
2017-10-24 22:35:12 +0900

    Refactor CreateGroupModal

--- src/components/CreateGroupModal.js
+++ src/components/CreateGroupModal.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import { withStyles, } from 'material-ui/styles';
+import React from 'react'
+import {connect} from 'react-redux'
+import { withStyles } from 'material-ui/styles'
+import Dialog, { DialogContent } from 'material-ui/Dialog'
+import TextField from 'material-ui/TextField'
+import List, { ListItem, ListItemText, } from 'material-ui/List'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import CloseIcon from 'material-ui-icons/Close'
+import Slide from 'material-ui/transitions/Slide'
+import CircularProgressButton from './CircularProgressButton'
+import Grid from 'material-ui/Grid'
+import Typography from 'material-ui/Typography'
+import Toolbar from 'material-ui/Toolbar'
 
-import Dialog, { DialogContent } from 'material-ui/Dialog';
-import TextField from 'material-ui/TextField';
-import List, { ListItem, ListItemText, } from 'material-ui/List';
-import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import CloseIcon from 'material-ui-icons/Close';
-import Slide from 'material-ui/transitions/Slide';
-import CircularProgressButton from './CircularProgressButton';
-import Grid from 'material-ui/Grid';
-import Typography from 'material-ui/Typography';
-import Toolbar from 'material-ui/Toolbar';
+import { withState, compose, withHandlers } from 'recompose'
 
-import ChipsArray from './ChipsArray';
-import {chatActionCreator} from '../actions';
+import ChipsArray from './ChipsArray'
+import {chatActionCreator} from '../actions'
 
 const styleSheet = theme => ({
     appBar: {
@@ -29,169 +30,135 @@ const styleSheet = theme => ({
     },
 });
 
+export const withSelectedUsers = withState('selectedUsers', 'setSelectedUsers', [])
+export const withSearchText = withState('searchText', 'setSearchText', '')
+export const withRoomName = withState('roomName', 'setRoomName', '')
+export const withModalHandlers = withHandlers({
+    handleDeleteChip: ( {selectedUsers, setSelectedUsers}) => data => {
+        const newSelectedUsers = selectedUsers.filter((u, index) => {
+            return u !== data.label
+        });
+        setSelectedUsers(newSelectedUsers)
+    },
+    handleAddChip: ({selectedUsers, setSelectedUsers}) => username => {
+        const newSelectedUsers = selectedUsers.slice();
+        newSelectedUsers.push(username)
+        setSelectedUsers(newSelectedUsers)
+    },
+    handleSearchTextChange: ({setSearchText}) => event => {
+        setSearchText(event.target.value)
+    },
+    handleRoomNameChange: ({setRoomName}) => event =>{
+        setRoomName(event.target.value)
+    },
+    handleCreateRoomClick: ({createRoom, selectedUsers, roomName}) => () => {
+        console.log(selectedUsers, roomName)
+        createRoom(selectedUsers, roomName)
+    }
+})
+ 
 
-class CreateGroupModal extends Component {
+export const CreateGroupModal = ( {ui, closeModal, entities, classes, selectedUsers, searchText, roomName, handleAddChip, handleRoomNameChange, handleCreateRoomClick, handleDeleteChip} ) => {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            selectedUsers: [],
-            roomName: '',
-            searchText: '',
+    const { showModal } = ui.createGroup;
+    const { friends } = entities;
+
+    const matchedUsernames = friends.all.filter((username) => {
+        const friend = friends.byUsername[username];
+        const regex = new RegExp(searchText);
+        return regex.test(friend.username) && !selectedUsers.includes(username);
+    });
+    
+    const chipData = selectedUsers.map((username, index) => {
+        return {
+            label: username,
+            key: index,
         }
+    });
 
-        this.handleAddChip = this.handleAddChip.bind(this);
-        this.handleDeleteChip = this.handleDeleteChip.bind(this);
-        this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
-        this.handleRoomNameChange = this.handleRoomNameChange.bind(this);
-        this.handleCreateRoomClick = this.handleCreateRoomClick.bind(this);
-    }
-
-    handleAddChip(username) {
-        const newSelectedUsers = this.state.selectedUsers.slice();
-        newSelectedUsers.push(username);
-        this.setState({selectedUsers: newSelectedUsers});
-    }
-
-    handleDeleteChip(data) {
-        const newSelectedUsers = this.state.selectedUsers.filter((u, index) => {
-            return u !== data.label;
-        });
-        this.setState({selectedUsers: newSelectedUsers});
-    }
-
-    handleSearchTextChange(event) {
-        this.setState({searchText: event.target.value});
-    }
-
-    handleRoomNameChange(event) {
-        this.setState({roomName: event.target.value});
-    }
-
-    handleCreateRoomClick() {
-        const {createRoom} = this.props;
-        const {selectedUsers, roomName} = this.state;
-        createRoom(selectedUsers, roomName);
-    }
-
-    render(){
-        const {
-            ui,
-            closeModal,
-            entities,
-            classes,
-        } = this.props;
-
-        const { showModal } = ui.createGroup;
-
-        const { friends } = entities;
-
-        const {selectedUsers, searchText, roomName} = this.state;
-
-        const matchedUsernames = friends.all.filter((username) => {
-            const friend = friends.byUsername[username];
-            const regex = new RegExp(searchText);
-            return regex.test(friend.username) && !selectedUsers.includes(username);
-        });
-
-        const chipData = selectedUsers.map((username, index) => {
-            return {
-                label: username,
-                key: index,
-            }
-        });
-
-        const matchedUserList = matchedUsernames.map((username, index) => {
-            return (
-                <ListItem button key={index}>
-                    <ListItemText
-                        primary={username}
-                        onClick={()=>{this.handleAddChip(username);}}
-                    />
-                </ListItem>
-            );
-        });
-
+    const matchedUserList = matchedUsernames.map((username, index) => {
         return (
-            <Dialog
-                open={showModal}
-                onRequestClose={ closeModal }
-                fullScreen
-                transition={<Slide direction="up" />}
-            >
-                <AppBar className={classes.appBar}>
-                    <Toolbar>
-                        <IconButton color="contrast" onClick={closeModal} aria-label="Close">
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography type="title" color="inherit" className={classes.flex}>
-                            Add your friends to a new group!
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <DialogContent className={classes.dialogContent}>
-                    <Grid container justify="center">
-                        <Grid item xs={5}>
-                            <TextField
-                                id="roomName"
-                                value={roomName}
-                                InputProps={{ placeholder: 'Type room name here!' }}
-                                onChange={this.handleRoomNameChange}
-                                fullWidth
-                                margin="normal"
-                            />
-                        </Grid>
-                        <Grid item xs={1}>
-                            <CircularProgressButton
-                                raised
-                                color="primary"
-                                onClick={() => {this.handleCreateRoomClick()}}
-                            >
-                                Go
-                            </CircularProgressButton>
-                        </Grid>
-                    </Grid>
-                    <Grid container justify="center">
-                        <Grid item xs={6}>
-                            <ChipsArray
-                                chipData={chipData}
-                                handleRequestDelete={this.handleDeleteChip}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid container justify="center">
-                        <Grid item xs={6}>
-                            <TextField
-                                id="search-friend"
-                                value={searchText}
-                                InputProps={{ placeholder: 'Search Your friends!' }}
-                                onChange={this.handleSearchTextChange}
-                                fullWidth
-                                margin="normal"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid container justify="center">
-                        <Grid item xs={6}>
-                            {matchedUserList.length > 0 ? (
-                                    <List>
-                                        {matchedUserList}
-                                    </List>
-                                ):
-                                <span>No matched User</span>
-                            }
-                        </Grid>
-                    </Grid>
-                </DialogContent>
-            </Dialog>
+            <ListItem button key={index}>
+                <ListItemText
+                    primary={username}
+                    onClick={()=>{handleAddChip(username);}}
+                />
+            </ListItem>
         );
-    }
+    });
+
+    return (
+        <Dialog
+            open={showModal}
+            onRequestClose={ closeModal }
+            transition={<Slide direction="up" />}
+        >
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <IconButton color="contrast" onClick={closeModal} aria-label="Close">
+                        <CloseIcon />
+                    </IconButton>
+                    <Typography type="title" color="inherit" className={classes.flex}>
+                        Add your friends to a new group!
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <DialogContent className={classes.dialogContent}>
+                <Grid container>
+                    <Grid item xs={10}>
+                        <TextField
+                            id="roomName"
+                            value={roomName}
+                            InputProps={{ placeholder: 'Type room name here!' }}
+                            onChange={handleRoomNameChange}
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <CircularProgressButton
+                            raised
+                            color="primary"
+                            onClick={() => {handleCreateRoomClick()}}
+                        >
+                            Go
+                        </CircularProgressButton>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <ChipsArray
+                            chipData={chipData}
+                            handleRequestDelete={handleDeleteChip}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            id="search-friend"
+                            value={searchText}
+                            InputProps={{ placeholder: 'Search Your friends!' }}
+                            onChange={this.handleSearchTextChange}
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        {matchedUserList.length > 0 ? (
+                                <List>
+                                    {matchedUserList}
+                                </List>
+                            ):
+                            <span>No matched User</span>
+                        }
+                    </Grid>
+                </Grid>
+            </DialogContent>
+        </Dialog>
+    )
 }
 
 const mapStateToProps = ({entities, ui}) => ({
     entities,
     ui,
-});
+})
 
 const mapDispatchToProps = (dispatch) => ({
     createRoom: (users, roomName = null) => {
@@ -204,8 +171,15 @@ const mapDispatchToProps = (dispatch) => ({
     closeModal: () => {
         dispatch(chatActionCreator.closeCreateGroupModal())
     }
-});
+})
 
-const StyledCreateGroupModal = withStyles(styleSheet)(CreateGroupModal);
+export const enhancer = compose(
+    connect(mapStateToProps, mapDispatchToProps), 
+    withStyles(styleSheet),
+    withSelectedUsers,
+    withRoomName,
+    withSearchText,
+    withModalHandlers,
+)
 
-export default connect(mapStateToProps, mapDispatchToProps)(StyledCreateGroupModal);
+export default enhancer(CreateGroupModal)
