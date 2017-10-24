3768e98a6a10df29b2c6f6fc167574e0b672edbd
Otsuki Hitoshi <hitochan777@gmail.com>
2017-10-24 23:56:03 +0900

    Add test for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Home(props) {
-    return <div> home! </div>
+export const Home = () => {
+    return <div>home!</div>
 }
 
---
+++ src/components/Home.spec.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import { ListItem, ListItemText } from 'material-ui/List'
+
+import { Home } from './Home'
+
+describe("Home", () => {
+    it("renders ", () => {
+        const wrapper = shallow(<Home />)
+        const home = <div>home!</div>
+        expect(wrapper).toContainReact(home)
+    })
+
+})
+
