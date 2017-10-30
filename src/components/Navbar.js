97110f02de372addae6a6448787832687720eeeb
Otsuki Hitoshi <hitochan777@gmail.com>
2017-10-30 16:31:27 +0900

    Add test for Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import React from 'react';
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import React from 'react'
+import { compose } from 'recompose'
 
-import { withStyles, } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
+import { withStyles, } from 'material-ui/styles'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
 
-import {logout as logoutAction} from '../actions/AuthActions';
+import { logout as logoutAction } from '../actions/AuthActions'
 
 const styleSheet = theme => ({
     root: {
@@ -18,42 +19,37 @@ const styleSheet = theme => ({
     flex: {
         flex: 1,
     },
-});
+})
 
-export function Navbar(props) {
-    const {authenticated, logout, classes} = props;
-    return(
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography type="title" color="inherit" className={classes.flex}>
-                        Chat
-                    </Typography>
-                    {authenticated ?
-                        ( <Button onClick={logout} color="contrast">Logout</Button>):
-                        ( <Button component={Link} to="/login" color="contrast">Login</Button>)
-                    }
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-}
+export const Navbar = ( { authenticated, logout, classes, } ) => (
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography type="title" color="inherit" className={classes.flex}>
+                    Chat
+                </Typography>
+                { authenticated ?
+                    ( <Button onClick={logout} color="contrast">Logout</Button> ):
+                    ( <Button component={Link} to="/login" color="contrast">Login</Button> )
+                }
+            </Toolbar>
+        </AppBar>
+    </div>
+)
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        logout: () => {
-            dispatch(logoutAction())
-        }
-    };
-};
+const mapDispatchToProps = (dispatch) => ({
+    logout: () => {
+        dispatch(logoutAction())
+    }
+})
 
-const mapStateToProps = ({session}) => ({
+const mapStateToProps = ( { session } ) => ({
     authenticated: session.authenticated,
-});
+})
 
-const StyledNavbar = withStyles(styleSheet)(Navbar);
+export const enhancer = compose(
+    withStyles(styleSheet),
+    connect(mapStateToProps, mapDispatchToProps)
+)
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(StyledNavbar);
+export default enhancer(Navbar)
