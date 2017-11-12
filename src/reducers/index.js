3ab4fa62d009ec93d030b5d7210a3ab0603f9281
Otsuki Hitoshi <hitochan777@gmail.com>
2017-11-13 00:32:52 +0900

    Initialize store on logout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,21 @@ import * as ui from './ui'
 import { sessionReducer as session } from 'redux-react-session'
 import * as entity from './entity'
 import * as domain from './domain'
+import { USER_LOGOUT } from '../actions/actionTypes'
 
-export const rootReducer = combineReducers({
+export const appReducer = combineReducers({
     session,
     ...entity,
     ...domain,
     ...ui,
 })
+
+const rootReducer = (state, action) => {
+    if (action.type === USER_LOGOUT) {
+        state = undefined
+    }
+
+    return appReducer(state, action)
+}
 
 export default rootReducer
