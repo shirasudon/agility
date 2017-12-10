c808fefbad6bc49eb3375fc96f52b35b0e16a78e
Otsuki Hitoshi <hitochan777@gmail.com>
2017-12-10 22:24:38 +0900

    Add test for sessionService

--- src/service/sessionService.js
+++ src/service/sessionService.js
@@ -1,15 +1,16 @@
 // @format
 
-import axios from 'axios'
-
+import httpService from './http'
 import { userAuth } from '../actions/AuthActions'
 
 // get the login information from the backend server and store it in redux `store`
-export default function init(store) {
-  return axios.get('/login').then(response => {
+export function init(store) {
+  return httpService.axios.get('/login').then(response => {
     const body = response.data
     if (body.logged_in === true) {
       store.dispatch(userAuth(body.user_id))
     }
   })
 }
+
+export default init
--- src/service/sessionService.spec.js
+++ src/service/sessionService.spec.js
@@ -1,11 +1,39 @@
 // @format
+import { init } from './sessionService'
+import httpService from './http'
 
 describe('login', () => {
-  it('sets user ID in auth part of redux store', () => {
-    fail()
+  beforeEach(() => {})
+  it('sets user ID in auth part of redux store if logged in', () => {
+    const store = {
+      dispatch: jest.fn(),
+    }
+    httpService.setAdapter(config => {
+      return new Promise((resolve, reject) => {
+        const mockData = { logged_in: true, user_id: 0 }
+        resolve({ data: mockData, status: 200 })
+      })
+    })
+    return init(store).then(() => {
+      expect(store.dispatch).toHaveBeenCalledWith({
+        type: 'USER_AUTH',
+        userId: 0,
+      })
+    })
   })
 
-  it('does not sets user ID in auth part of redux store', () => {
-    fail()
+  it('does not sets user ID in auth part of redux store if not logged in', () => {
+    const store = {
+      dispatch: jest.fn(),
+    }
+    httpService.setAdapter(config => {
+      return new Promise(resolve => {
+        const mockData = { logged_in: false, user_id: 0 }
+        resolve({ data: mockData, status: 200 })
+      })
+    })
+    return init(store).then(() => {
+      expect(store.dispatch).not.toHaveBeenCalled()
+    })
   })
 })
